c1cc98ac25d46de83c0853fded1e8a8fdc3fcf66
Zahra <newzahrain2024@gmail.com>
2024-10-24 13:18:53 -0700

    useraccount creation with django default forms

--- myproject/settings.py
+++ myproject/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # local
     'myhomepage',
+    'accounts', 
     
 ]
 
@@ -126,4 +128,5 @@ STATICFILES_DIRS = [BASE_DIR/'static']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+LOGIN_REDIRECT_URL = 'homepage'
+LOGOUT_REDIRECT_URL = 'homepage'
